17b20d3cec36097f664baf937928e3acad5909e8
mlutegar <mlutegar@gmail.com>
2024-05-09 14:33:55 -0300

    melhorando o botão de download e upload

--- src/components/ArquivosServico/ArquivoServico.jsx
+++ src/components/ArquivosServico/ArquivoServico.jsx
@@ -9,22 +9,38 @@ import {useState} from "react";
 const uploadArquivo = () => {
     const input = document.createElement("input");
     input.type = "file";
+    input.onchange = () => {
+        if (input.files.length > 0) {
+            let arquivo = input.files[0];
+            // Adicione aqui a validação do arquivo se necessário
+            salvarArquivo(arquivo);
+        }
+    }
     input.click();
+    // Garantir que o input seja removido após o uso
+    input.remove();
 }
 
-// baixarArquivo: função que baixa um arquivo a partir de um link e o salva no computador do usuário assim que o botão é clicado
-// parametros: link : string - link do arquivo a ser baixado
-const baixarArquivo = (link) => {
-    fetch(link)
-        .then(response => {
-            response.blob().then(blob => {
-                let url = window.URL.createObjectURL(blob);
-                let a = document.createElement('a');
-                a.href = url;
-                a.download = link.split("/").pop();
-                a.click();
-            });
-        });
+
+// salvarArquivo: função que salva um arquivo escolhido pelo usuário no localstorage do navegador
+// parametros:  arquivo : File - arquivo a ser salvo
+const salvarArquivo = (arquivo) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+        localStorage.setItem("img", reader.result);
+    }
+    reader.readAsDataURL(arquivo);
+}
+
+
+// baixarArquivo: função que baixa um arquivo do localstorege a partir de seu id no localstorage, assim que o usuário clica no botão de download
+const baixarArquivo = () => {
+    const img = localStorage.getItem("img");
+    const a = document.createElement
+    ("a");
+    a.href = img;
+    a.download = "img";
+    a.click();
 }
 
 // // habilitarRelatorio: função que habilita o botão de download do relatório assim que o relatório é enviado, alterando o estado do relatório
