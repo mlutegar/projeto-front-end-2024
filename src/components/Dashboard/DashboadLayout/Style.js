2637b39082a4212669b4eb849d91aac075ae5969
mlutegar <mlutegar@gmail.com>
2024-05-19 20:53:07 -0300

    feat: adicionando responsividade no dashboard

--- src/components/Dashboard/DashboadLayout/Style.js
+++ src/components/Dashboard/DashboadLayout/Style.js
@@ -29,7 +29,38 @@ const DashboadLayoutStyle = styled.div`
     .atualizacao{
         grid-area: atualizacao;
     }
+
+    // Telas menores que 1200px (Desktop pequenos)
+    @media only screen and (max-width: 1200px) {
+        .container{
+            display: grid;
+            grid-template-areas:
+            "solicitacao solicitacao"
+            "calibracao calibracao"
+            "informacao informacao"
+            "atualizacao atualizacao"
+        ;
+            grid-template-columns: 2fr 1.5fr;
+            gap: 20px;
+            flex-wrap: wrap;
+        }
+    }
     
+    // Telas menores que 769px (Tablets)
+    @media only screen and (max-width: 769px) {
+        .container{
+            display: grid;
+            grid-template-areas:
+            "solicitacao"
+            "calibracao"
+            "informacao"
+            "atualizacao"
+        ;
+            grid-template-columns: 1fr;
+            gap: 20px;
+            flex-wrap: wrap;
+        }
+    }
     
 `;
 
